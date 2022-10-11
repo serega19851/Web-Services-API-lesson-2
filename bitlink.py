e8088a2a096f94ae63add39b2c30e1d6e03de13d
Сергей Антонов <antonovseantonoff@yandex.ru>
2022-10-11 10:51:52 +0300

    attempt 3

--- bitlink.py
+++ bitlink.py
@@ -7,8 +7,9 @@ from dotenv import load_dotenv
 def shorten_link(token, url):
     headers = {'Authorization': f'Bearer {token}'}
     user_url = {'long_url': url}
-    response = requests.post('https://api-ssl.bitly.com/v4/bitlinks',
-                             headers=headers, json=user_url)
+    response = requests.post(
+        'https://api-ssl.bitly.com/v4/bitlinks', headers=headers, json=user_url
+    )
     return response.json()['id']
 
 
@@ -18,7 +19,8 @@ def count_clicks(token, link):
     response = requests.get(
         'https://api-ssl.bitly.com/v4/bitlinks/'
         f'{user_link.netloc}{user_link.path}/clicks/summary',
-        headers=headers, params={"unit": "month", "units": -1})
+        headers=headers, params={"unit": "month", "units": -1}
+    )
     return response.json()['total_clicks']
 
 
@@ -27,20 +29,21 @@ def is_bitlink(token, url):
     headers = {'Authorization': f'Bearer {token}'}
     response = requests.get(
         f'https://api-ssl.bitly.com/v4/bitlinks/{user_url.netloc}'
-        f'{user_url.path}', headers=headers)
+        f'{user_url.path}', headers=headers
+    )
     return response.ok
 
 
 def main():
     load_dotenv()
-    token_bitlay = os.getenv("TOKEN_BITLY")
+    bitly_token = os.getenv("BITLY_TOKEN")
     user_input = input('Введите ссылку: ')
-    if is_bitlink(token_bitlay, user_input):
-        return (f'По вашей ссылки прошли: '
-                f'{count_clicks(token_bitlay, user_input)}'
-                f' раз(а)')
-    return (f'Битлинк: '
-            f'{shorten_link(token_bitlay, user_input)}')
+    if is_bitlink(bitly_token, user_input):
+        return (
+            f'По вашей ссылки прошли: {count_clicks(bitly_token, user_input)}'
+            f' раз(а)'
+        )
+    return f'Битлинк: {shorten_link(bitly_token, user_input)}'
 
 
 if __name__ == '__main__':
